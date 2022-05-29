c24af353cfadd2a7bec06a91b133dd692ad36a7f
alexey.bondarenko <alexey.bondarenko@team.wrike.com>
2022-05-29 11:39:54 +0300

    Tests

---
+++ src/main/java/Account.java
@@ -0,0 +1,27 @@
+/**
+ * Author: Alexey Bondarenko
+ * Date: 29.05.2022
+ */
+public class Account {
+
+    private final String name;
+
+    public Account(String name) {
+        this.name = name;
+    }
+
+    public boolean checkNameToEmboss() {
+        return checkLengthName() && checkSpacesIntoName();
+    }
+
+    private boolean checkLengthName() {
+        return name.length() >= 3 && name.length() <= 19;
+    }
+
+    private boolean checkSpacesIntoName() {
+        int countOfSpaces = name.length() - name.replaceAll(" ", "").length();
+        boolean isSpaceInTheRightPlace = !(name.startsWith(" ") || name.endsWith(" "));
+        return countOfSpaces == 1 && isSpaceInTheRightPlace;
+    }
+
+}
